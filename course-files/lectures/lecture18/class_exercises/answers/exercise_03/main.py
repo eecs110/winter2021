b07142011d7bf4a0b3ff0e332c29c7f547e5c1c8
Sarah <vanwars@gmail.com>
2021-02-26 09:12:26 -0600

    reordg

---
+++ course-files/lectures/lecture18/class_exercises/answers/exercise_03/main.py
@@ -0,0 +1,69 @@
+'''
+Some instructions:
+1. Documentation
+   Here's a Tutorial for Flask:
+    https://pythonspot.com/flask-hello-world/
+
+2. First install flask (you'll only have to do this once):
+   Note that before you begin, you will have to 
+   install flask on the command line as follows:
+     pip3 install flask
+
+3. Running this file will start a web server.
+   To actually execute the code, open your web browser
+   and navigate to: http://127.0.0.1:5000/ 
+
+4. If you actually want to turn this into a website, read the
+   PythonAnywhere documentation, which teaches you how to upload
+   your files so that you can serve them as a web page.
+
+'''
+
+from flask import Flask
+app = Flask(__name__)
+import random
+    
+photos = [
+    'static/kittens.jpg',
+    'static/beagle-puppy.jpg',
+    'static/kangaroo.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/1/18/Vombatus_ursinus_-Maria_Island_National_Park.jpg'
+]
+
+@app.route("/")
+def home():
+    # this page's job is to pick a random photo from a list
+    return '<img src="{0}" />'.format(random.choice(photos))
+
+@app.route("/tuesday")
+def reminder1():
+    return "Drinking water is a good thing to do."
+
+@app.route("/wednesday")
+def reminder2():
+    return "Don't forget to eat your vegetables"
+
+@app.route("/friday")
+def friday_page():
+    template = '''
+        <html>
+            <head>
+                <title>{title}</title>
+            </head>
+            <body>
+                <h1>{title}</h1>
+                <p>{message}</p>
+                <img style="width:500px;" src="{image_url}" />
+            </body>
+        </html>
+    '''
+    
+    return template.format(
+        title="Happy Friday",
+        message="Hope you have a nice weekend!",
+        image_url=random.choice(photos)
+    )
+
+
+if __name__ == "__main__":
+    app.run()
